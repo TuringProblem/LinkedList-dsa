3a5c6a90a426ede51111cfb88c64d550e6a8ce18
Override <122590935+TuringProblem@users.noreply.github.com>
2024-04-06 19:03:09 -0400

    feat: Updated the LinkedList
    Still need to add more comments, but overall this is all of the methods, and constructors for the LinkedList.

--- LinkedList.java
+++ LinkedList.java
@@ -50,6 +50,18 @@ public class LinkedList {
         }
         return temp;
     }
+
+    public Node removeFirst() {
+        if (length == 0) return null; //if the length of the list is - 0 - then return null -> can also do if head == null, return null;
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        length--;
+        if (length == 0) { //now if the length  == 0 then all we need to do is push tail to null value
+            tail = null; //need to do this because tail will still be pointing to the node that is now gone
+        }
+        return temp;
+    }
     public void  prepend(int value) {
         //create a new Node
         Node newNode = new Node(value);
@@ -66,22 +78,66 @@ public class LinkedList {
         }
         length++;
     }
-    public Node removeFirst() {
-        if (length == 0) return null;
-        Node temp = head;
-        head = head.next;
-        temp.next = null;
-        length--;
-        if (length == 0) {
-            tail = null;
-        }
-        return temp;
+    public Node get(int index) {
+        if (index < 0 || index >= length) return null; //making sure that the index set by the user is in range
+       Node temp = head;
+       for (int i = 0; i < index; i++) { //iterate through the specific index.
+           temp = temp.next;
+       }
+       return temp;
     }
-    public boolean insert(int index, int value) {
-        Node newNode = new Node(value);
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+        if (temp != null) {
+            temp.value = value;
+            return true;
+        }
         return false;
     }
 
+    public boolean insert(int index, int value) {
+        if (index < 0 || index > length) return false;
+        if (index == 0) { //if adding something to the beginning
+            prepend(value);
+            return true;
+        }
+        if (index == length){ //if adding something to the end
+            append(value);
+            return true;
+        }
+        Node newNode = new Node(value);
+        Node temp = get(index - 1);
+        newNode.next  = temp.next;
+        temp.next = newNode;
+        length++;
+        return true;
+    }
+
+    public Node remove(int index) {
+        if (index < 0 || index >= length) return null;
+        if (index == 0) return removeFirst();
+        if (index == length - 1) return removeLast();
+        Node prev = get(index - 1);
+        Node temp = prev.next;
+        prev.next = temp.next; //This allows the Node that prev is pointing to -> to remove the node that temp is pointing to.
+        temp.next = null; //This makes the Node that temp is on to point at null
+        length--;
+        return temp;
+    }
+
+    public void reverse() { //Reverses the linked list by using temp, after, and before Nodes
+        Node temp = head;
+        head = tail;
+        tail = temp;
+        Node after = temp.next;
+        Node before = null;
+        for (int i = 0; i < length; i++) { // This for loop iterates through the three Temporary Nodes created.
+            after = temp.next;
+            temp.next = before;
+            before = temp;
+            temp = after;
+        }
+    }
     public void printList() {
         Node temp = head;
         while(temp != null) {
